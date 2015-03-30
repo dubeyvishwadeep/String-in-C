eb36baddefc955bf611ba0f912b86bc4ad26a4b9
vishwadeep dubey <vishwadeepdubey56@gmail.com>
2015-03-30 11:18:14 +0530

    string
    simple program demonstration printing of a strig

---
+++ string.c
@@ -0,0 +1,14 @@
+/* program to demonstration printing of a string*/
+#include<stdio.h>
+int main()
+{
+char name[]="vishwadeep";
+int i=0;
+while(i<=9)
+{
+printf("%c",name[i]);
+i++;
+}
+printf("\n");
+return 0;
+}
